efb9c969e760e5cf6cc3c0ce987042f07f330502
Peter Olsen <pto@me.com>
2020-02-17 19:48:12 -0500

    more comments

--- ch01/prog.lua
+++ ch01/prog.lua
@@ -5,7 +5,15 @@ print(a + 2)
 
 --[[
 print(42)       -- no action (commented out)
+]]
+
+print("After first comment")
+
+--[[
+print(101)      -- also commented out
 --]]
+
+print("End of comments")
 
 ---[[
 print(10)       --> 10
